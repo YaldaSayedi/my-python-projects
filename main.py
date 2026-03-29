c3b40aa5adf3fdee68678178a38835c506a756b3
Yalda Sayedi <sayediyalda@gmail.com>
2026-03-29 23:45:14 +0200

    Nested If Statement

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
-number= int(input("Enter your number: "))
-if number % 2 == 0:
-    print(number, "is even")
+print("Welcome to the rollercoaster! ")
+height= int(input("What is your height?"))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age= int(input("What is your age? ?"))
+    if age >= 18:
+        print("You should pay 10$")
+    else:
+        print("You should pay 5$")
+
 else:
-    print(number, "is odd")
+    print("I am sorry but you can not ride the rollercoaster!")
