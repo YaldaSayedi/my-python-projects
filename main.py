0502b2b6fb5e4bb0df227af9bca525f399e6f733
Yalda Sayedi <sayediyalda@gmail.com>
2026-04-21 14:55:44 +0200

    Rock, Paper, Scissors

--- main.py
+++ main.py
@@ -1,48 +1,54 @@
-print(r'''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
-|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to the treasure Island.\nYour Mission is to find the treasure.")
-Choice1= input('You\'re at a crossroad, where do you want to go? '
-                'Type "left" or "right". \n').lower()
-if Choice1 == "left":
-    Choice2= input('You\'ve come to a lake. '
-                    'There is an island in the middle of the lake. '
-                    'Type "wait" to wait for a boat. '
-                    'Type "swim" to swim across.\n').lower()
-    if Choice2 == "wait":
-        Choice3= input("You arrive at the island unharmed. "
-                        "There is house with 3 doors. One red, "
-                        "one yellow and one blue. "
-                        "Which colour do you choose?\n").lower()
-        if Choice3 == "red":
-            print("Burned by fire \n Game Over.")
-        elif Choice3 == "yellow":
-            print("You Win!")
-        elif Choice3 == "blue":
-            print("Eaten by beasts.\nGame Over.")
-        else:
-            print("You chose a door that doesn't exist. Game Over.")
-    else:
-        print("Attacked by trout.\nGame Over.")
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-else:
-    print("Fall into a hole.\nGame Over.")
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+
+user_choice= int(input("What do you choose? type 0 for Rock, 1 for paper, 2 for scissors: "))
+if user_choice >=0 and user_choice <=2:
+    print("Your choice", game_images[user_choice])
+
+computer_choice = random.randint(0,2)
+print("Computer Choice", game_images[computer_choice])
+
+if user_choice >=3 or user_choice <0:
+    print("Invalid Choice")
+elif user_choice ==0 and computer_choice == 2:
+    print("You Won!")
+elif computer_choice == 0 and computer_choice==2:
+    print("You Lost!")
+elif user_choice > computer_choice:
+    print("You Won!")
+elif computer_choice > user_choice:
+    print(" You lsot!")
+elif user_choice == computer_choice:
+    print("Its a draw!")
+
+
+
+
+
+
